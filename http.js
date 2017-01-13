4c8579e993c7fec49383b008acb517cb92bcfdb8
Ronny Springer <springer.ronny@gmail.com>
2017-01-13 12:49:56 +0100

    simple http tryout to google news

--- http.js
+++ http.js
@@ -1,5 +1,19 @@
 'use strict';
 
+var request = require('request');
+
 export.get = function (event, context, callback) {
-	return null;
+	var URI = 'https://news.google.com/news';
+	
+	request(URI, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			console.debug('HTTP request passed');
+			contect.done(null, { html : body });
+		} else {
+			console.debug('Error while HTTP processing.');
+			context.done(null, { message : error });
+		}
+	});
+
+	return;
 }
