03131d3295a1f85dd9ef1b0aa14a51ef69e3545f
Ronny Springer <springer.ronny@gmail.com>
2017-01-13 12:58:16 +0100

    fix typo

--- http.js
+++ http.js
@@ -8,7 +8,7 @@ export.get = function (event, context, callback) {
 	request(URI, function (error, response, body) {
 		if (!error && response.statusCode == 200) {
 			console.debug('HTTP request passed');
-			contect.done(null, { html : body });
+			context.done(null, { html : body });
 		} else {
 			console.debug('Error while HTTP processing.');
 			context.done(null, { message : error });
@@ -16,4 +16,4 @@ export.get = function (event, context, callback) {
 	});
 
 	return;
-}
+};
